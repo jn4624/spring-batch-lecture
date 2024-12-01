a15a71f821403b4133f950cdc729f1997adf0d84
jn4624 <jn4624@gmail.com>
2024-12-01 22:39:33 +0900

    initial commit

---
+++ src/test/java/com/io/springbatch/SpringBatchLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
