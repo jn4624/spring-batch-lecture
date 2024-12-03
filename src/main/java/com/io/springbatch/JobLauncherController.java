23cdfd608a6006e80621596822231830418bdc48
jn4624 <jn4624@gmail.com>
2024-12-04 01:26:22 +0900

    도메인 이해 - JobLauncher

---
+++ src/main/java/com/io/springbatch/JobLauncherController.java
@@ -0,0 +1,53 @@
+package com.io.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.boot.autoconfigure.batch.BasicBatchConfigurer;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequiredArgsConstructor
+public class JobLauncherController {
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    private final BasicBatchConfigurer basicBatchConfigurer;
+
+    @PostMapping("/sync")
+    public String sync(@RequestBody Member member) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("id", member.getId())
+                .addDate("date", new Date())
+                .toJobParameters();
+
+        jobLauncher.run(job, jobParameters);
+        return "sync batch completed";
+    }
+
+    @PostMapping("/async")
+    public String async(@RequestBody Member member) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("id", member.getId())
+                .addDate("date", new Date())
+                .toJobParameters();
+
+        SimpleJobLauncher simpleJobLauncher = (SimpleJobLauncher) basicBatchConfigurer.getJobLauncher();
+        simpleJobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        simpleJobLauncher.run(job, jobParameters);
+
+        return "async batch completed";
+    }
+}
---
+++ src/main/java/com/io/springbatch/Member.java
@@ -0,0 +1,8 @@
+package com.io.springbatch;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private String id;
+}
