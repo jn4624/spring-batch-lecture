780b9af3822ce6513971314d7c88f01272e84aea
jn4624 <jn4624@gmail.com>
2024-12-08 21:00:00 +0900

    ItemReader - JpaCursorItemReader

---
+++ src/main/java/com/io/springbatch/Customer.java
@@ -0,0 +1,18 @@
+package com.io.springbatch;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String birthdate;
+}
