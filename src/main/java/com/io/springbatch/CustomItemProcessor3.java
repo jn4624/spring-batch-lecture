6212f086a38baa485c0147748b1014a480658eff
jn4624 <jn4624@gmail.com>
2024-12-09 15:47:38 +0900

    ItemProcessor - ClassifierCompositeItemProcessor

---
+++ src/main/java/com/io/springbatch/CustomItemProcessor3.java
@@ -0,0 +1,11 @@
+package com.io.springbatch;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomItemProcessor3 implements ItemProcessor<ProcessorInfo, ProcessorInfo> {
+    @Override
+    public ProcessorInfo process(ProcessorInfo processorInfo) throws Exception {
+        System.out.println("CustomItemProcessor3");
+        return processorInfo;
+    }
+}
